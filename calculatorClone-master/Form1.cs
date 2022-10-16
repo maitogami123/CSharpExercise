6195ddf4b0671a28097c71718db4dbcce47c4ef6
tuyenkieuminh <87085555+tuyenkieuminh@users.noreply.github.com>
2022-10-16 16:18:22 +0700

    Add files via upload

---
+++ calculatorClone-master/Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorClone
+{
+    public partial class Form1 : Form
+    {
+        double A;
+        string Operation="";
+        bool tiep=false;
+        bool g=false;
+        bool delete_lock=false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*private void n1_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "0" && label3.Text != null)
+            {
+                label3.Text = "1";
+            }
+            else
+            {
+                label3.Text = label3.Text + "1";
+            }
+        }
+
+        private void n2_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "0" && label3.Text != null)
+            {
+                label3.Text = "2";
+            }
+            else
+            {
+                label3.Text = label3.Text + "2";
+            }
+        }
+
+        private void n3_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "0" && label3.Text != null)
+            {
+                label3.Text = "3";
+            }
+            else
+            {
+                label3.Text = label3.Text + "3";
+            }
+        }
+
+        private void n4_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "0" && label3.Text != null)
+            {
+                label3.Text = "4";
+            }
+            else
+            {
+                label3.Text = label3.Text + "4";
+            }
+        }
+
+        private void n5_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "0" && label3.Text != null)
+            {
+                label3.Text = "5";
+            }
+            else
+            {
+                label3.Text = label3.Text + "5";
+            }
+        }
+
+        private void n6_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "0" && label3.Text != null)
+            {
+                label3.Text = "6";
+            }
+            else
+            {
+                label3.Text = label3.Text + "6";
+            }
+        }
+
+        private void n7_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "0" && label3.Text != null)
+            {
+                label3.Text = "7";
+            }
+            else
+            {
+                label3.Text = label3.Text + "7";
+            }
+        }
+
+        private void n8_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "0" && label3.Text != null)
+            {
+                label3.Text = "8";
+            }
+            else
+            {
+                label3.Text = label3.Text + "8";
+            }
+        }
+
+        private void n9_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "0" && label3.Text != null)
+            {
+                label3.Text = "9";
+            }
+            else
+            {
+                label3.Text = label3.Text + "9";
+            }
+        }
+        private void n0_Click(object sender, EventArgs e)
+        {
+            if (label3.Text != "0" && label3.Text != null)
+            {
+                label3.Text = label3.Text + "0";
+            }
+        }*/
+        private void MyButtonClick(object sender, EventArgs e)
+        {
+            delete_lock = false;
+            if (tiep == false && Operation=="")
+            {
+                label2.Text = "0";
+            }
+            if (g == true)
+            {
+                label3.Text = "0";
+                g= false;
+            }
+            string buttonText = ((Button)sender).Text;
+            switch(buttonText)
+            {
+                case "0":
+                    if (label3.Text != "0" && label3.Text != null)
+                    {
+                        label3.Text = label3.Text + "0";
+                    }
+                    break;
+                case "1":
+                    if (label3.Text == "0" && label3.Text != null)
+                    {
+                        label3.Text = "1";
+                    }
+                    else
+                    {
+                        label3.Text = label3.Text + "1";
+                    }
+                    break;
+                case"2":
+                    if (label3.Text == "0" && label3.Text != null)
+                    {
+                        label3.Text = "2";
+                    }
+                    else
+                    {
+                        label3.Text = label3.Text + "2";
+                    }
+                    break ;
+                case "3":
+                    if (label3.Text == "0" && label3.Text != null)
+                    {
+                        label3.Text = "3";
+                    }
+                    else
+                    {
+                        label3.Text = label3.Text + "3";
+                    }
+                    break;
+                case"4":
+                    if (label3.Text == "0" && label3.Text != null)
+                    {
+                        label3.Text = "4";
+                    }
+                    else
+                    {
+                        label3.Text = label3.Text + "4";
+                    }
+                    break;
+                case"5":
+                    if (label3.Text == "0" && label3.Text != null)
+                    {
+                        label3.Text = "5";
+                    }
+                    else
+                    {
+                        label3.Text = label3.Text + "5";
+                    }
+                    break;
+                case"6":
+                    if (label3.Text == "0" && label3.Text != null)
+                    {
+                        label3.Text = "6";
+                    }
+                    else
+                    {
+                        label3.Text = label3.Text + "6";
+                    }
+                    break;
+                case "7":
+                    if (label3.Text == "0" && label3.Text != null)
+                    {
+                        label3.Text = "7";
+                    }
+                    else
+                    {
+                        label3.Text = label3.Text + "7";
+                    }
+                    break ;
+                case"8":
+                    if (label3.Text == "0" && label3.Text != null)
+                    {
+                        label3.Text = "8";
+                    }
+                    else
+                    {
+                        label3.Text = label3.Text + "8";
+                    }
+                    break;
+                case"9":
+                    if (label3.Text == "0" && label3.Text != null)
+                    {
+                        label3.Text = "9";
+                    }
+                    else
+                    {
+                        label3.Text = label3.Text + "9";
+                    }
+                    break;
+            }
+        }
+        /*private void bad_Click(object sender, EventArgs e)
+        {
+
+            if (Operation == "")
+            {
+                A = Convert.ToDouble(label3.Text);
+                label2.Text = label3.Text + " +";
+                label3.Text = "0";
+                Operation = "+";
+            }
+        }
+
+        private void bsub_Click(object sender, EventArgs e)
+        {
+            if (Operation == "")
+            {
+                A = Convert.ToDouble(label3.Text);
+                label2.Text = label3.Text + " -";
+                label3.Text = "0";
+                Operation = "-";
+            }
+        }
+
+        private void bmul_Click(object sender, EventArgs e)
+        {
+            if (Operation == "")
+            {
+                A = Convert.ToDouble(label3.Text);
+                label2.Text = label3.Text + " *";
+                label3.Text = "0";
+                Operation = "*";
+            }
+        }
+
+        private void bdiv_Click(object sender, EventArgs e)
+        {
+            if (Operation == "")
+            {
+                A = Convert.ToDouble(label3.Text);
+                label2.Text = label3.Text + " /";
+                label3.Text = "0";
+                Operation = "/";
+            }
+        }*/
+        private void basic_math(object sender, EventArgs e)
+        {
+            delete_lock = false;
+            string buttonText = ((Button)sender).Text;
+            switch (buttonText)
+            {
+                case "+":
+                    if (Operation == "")
+                    {
+                        A = Convert.ToDouble(label3.Text);
+                        label2.Text = label3.Text + " +";
+                        label3.Text = "0";
+                        Operation = "+";
+                    }
+                    break;
+                case "-":
+                    if (Operation == "")
+                    {
+                        A = Convert.ToDouble(label3.Text);
+                        label2.Text = label3.Text + " -";
+                        label3.Text = "0";
+                        Operation = "-";
+                    }
+                    break;
+                case "*":
+                    if (Operation == "")
+                    {
+                        A = Convert.ToDouble(label3.Text);
+                        label2.Text = label3.Text + " *";
+                        label3.Text = "0";
+                        Operation = "*";
+                    }
+                    break ;
+                case "/":
+                    if (Operation == "")
+                    {
+                        A = Convert.ToDouble(label3.Text);
+                        label2.Text = label3.Text + " /";
+                        label3.Text = "0";
+                        Operation = "/";
+                    }
+                    break;
+            }
+        }
+
+        private void bequ_Click(object sender, EventArgs e)
+        {
+            double B;
+            double Result;
+            B = Convert.ToDouble(label3.Text);
+
+            if (Operation == "+")
+            {
+                Result = (A + B);
+                label2.Text = label2.Text+" "+B+" =";
+                label3.Text = Convert.ToString(Result);
+                A = Result;
+            }
+            if (Operation == "-")
+            {
+                Result = (A - B);
+                label2.Text = label2.Text + " " + B + " =";
+                label3.Text = Convert.ToString(Result);
+                A = Result;
+            }
+            if (Operation == "*")
+            {
+                Result = (A * B);
+                label2.Text = label2.Text + " " + B + " =";
+                label3.Text = Convert.ToString(Result);
+                A = Result;
+            }
+            if (Operation == "/")
+            {
+                if (B == 0)
+                {
+                    label3.Text = "Cannot divide by zero";
+
+                }
+                else
+                {
+                    Result = (A / B);
+                    label2.Text = label2.Text + " " + B + " =";
+                    label3.Text = Convert.ToString(Result);
+                    A = Result;
+                }
+            }
+            if (Operation == "")
+            {
+                label2.Text = label3.Text + " =";
+            }
+            Operation = "";
+            tiep=false;
+            g=true;
+            delete_lock = true;
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            if (delete_lock == false)
+            {
+                if (label3.Text.Length > 1)
+                {
+                    string n = label3.Text.Remove(label3.Text.Length - 1);
+                    label3.Text = n;
+                }
+                else
+                {
+                    label3.Text = "0";
+                }
+            }
+        }
+    }
+}
